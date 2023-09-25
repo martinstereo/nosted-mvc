07a8a6edc5c6ccc0f326c122234a8e3d3ecd871d
Buse Yilmaz <112386569+buseliiik@users.noreply.github.com>
2023-09-25 11:41:12 +0200

    Add files via upload
    add a serviceSkjema file in model

---
+++ nosted-dotnet.MVC/Models/ServiceSkjema/ServiceSkjemaViewModel.cs
@@ -0,0 +1,29 @@
+﻿namespace nosted_dotnet.MVC.Models.ServiceSkjema
+{
+    public class ServiceSkjemaViewModel
+    {
+        public required string Mechanic { get; set; }
+        public bool IsAdministrator { get; set; }
+        public required string SerialNumber { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public decimal ConsumedHours { get; set; }
+        public required string ImageUrl { get; set; }
+        public required string MechanicComment { get; set; }
+        public required string CustomerComment { get; set; }
+
+        public required string FutureMaintenance { get; set; }
+        public int ServiceSkjemaId { get; set; }
+
+        public required string CustomerName { get; set; }
+        public required string CustomerEmail { get; set; }
+        public required string CustomerStreet { get; set; }
+        public required string CustomerCity { get; set; }
+        public required string CustomerZipcode { get; set; }
+        public  required string CustomerTelephoneNumber { get; set; }
+
+    }
+
+  
+}
+
+
