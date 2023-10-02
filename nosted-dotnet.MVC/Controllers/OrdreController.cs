de513b546914675f3b876b96b9c914c85ae5006d
martinstereo <martinsteiro02@gmail.com>
2023-10-02 12:41:56 +0200

    Tabell med skjema som kan legge til rader,

--- nosted-dotnet.MVC/Controllers/OrdreController.cs
+++ nosted-dotnet.MVC/Controllers/OrdreController.cs
@@ -10,13 +10,25 @@ namespace nosted_dotnet.MVC.Controllers
 {
     public class OrdreController : Controller
     {
+        private List<OrdreRad> OrderDataTable = new();
+        
         public IActionResult Index()
         {
-            var model = new OrdreViewModel();
-            return View(model);
+            return View(OrderDataTable);
         }
 
         //HTTP post etc
+        [HttpPost]
+        public IActionResult AddRow(OrdreRad row)
+        {
+            // Add the submitted row to the table
+            OrderDataTable.Add(row);
+
+            // Redirect back to the index page
+
+            return View("Index", OrderDataTable);
+        }
+
 
     }
     
