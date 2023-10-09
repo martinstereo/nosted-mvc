0182ada0e86372acbf5b6a40c23d2d6c7262e548
Busenur Yilmaz <busenury@uia.no>
2023-10-09 14:26:16 +0200

    Lagde delete knapp i bruker form. Kommer det en confirmaation dialog mens du prøver å delete. "are you sure to delete this"

--- nosted-dotnet.MVC/Models/Bruker/BrukerViewModel.cs
+++ nosted-dotnet.MVC/Models/Bruker/BrukerViewModel.cs
@@ -14,6 +14,11 @@ namespace nosted_dotnet.MVC.Models.Bruker
         public string? Adresse { get; set; }
         public string? Telefonnummer { get; set; }
         public string? Stilling { get; set; }
+
+        public string? Kundesenter { get; set; }
+        public string? Administrator { get; set; }
+        public string? Mekaniker { get; set; }
+
        
 
     }
