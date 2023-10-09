d4e92709967aba18a6e26aff734dcbb3a25b97d6
Busenur Yilmaz <busenury@uia.no>
2023-10-09 12:55:31 +0200

    lagde brukeradministrasjon side koblet den med knappen i home side.

---
+++ nosted-dotnet.MVC/Models/Bruker/BrukerViewModel.cs
@@ -0,0 +1,21 @@
+﻿using bacit_dotnet.MVC.Controllers;
+using bacit_dotnet.MVC.Models.CheckList;
+using nosted_dotnet.MVC.Controllers;
+using nosted_dotnet.MVC.Models.ServiceSkjema;
+using System.Net.Http.Headers;
+
+namespace nosted_dotnet.MVC.Models.Bruker
+{
+    public class BrukerRad
+    {
+        public int BrukerID { get; set; }
+        public string? Navn { get; set; }
+        public string? Etternavn { get; set; }
+        public string? Adresse { get; set; }
+        public string? Telefonnummer { get; set; }
+        public string? Stilling { get; set; }
+       
+
+    }
+}
+    
