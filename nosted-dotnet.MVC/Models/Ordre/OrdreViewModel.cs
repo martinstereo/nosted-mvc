662fd41898b62e8de7e4b7cc4fdbf6c569bd692a
annetteab <annetteab98@gmail.com>
2023-10-12 21:44:46 +0200

    la til view for ny ordre, samt knapp for å åpne ordreskjema

--- nosted-dotnet.MVC/Models/Ordre/OrdreViewModel.cs
+++ nosted-dotnet.MVC/Models/Ordre/OrdreViewModel.cs
@@ -2,6 +2,7 @@
 using bacit_dotnet.MVC.Models.CheckList;
 using nosted_dotnet.MVC.Controllers;
 using nosted_dotnet.MVC.Models.ServiceSkjema;
+using System.ComponentModel.DataAnnotations;
 using System.Net.Http.Headers;
 
 namespace nosted_dotnet.MVC.Models.Ordre
@@ -15,6 +16,46 @@ namespace nosted_dotnet.MVC.Models.Ordre
         public ServiceSkjemaController? ServiceSkjema { get; set; }
         public CheckListController? Sjekkliste { get; set; }
         public bool IsDone { get; set; }
+    }
+   
+    public class OrdreSkjema
+    {
+        public string OrdreSkjemaID { get; set; }
+
+        [Required(ErrorMessage = "Vennligst oppgi kundenavn.")]
+        public string Fornavn { get; set; }
+
+        [Required(ErrorMessage = "Vennligst oppgi kundenavn.")]
+        public string Etternavn { get; set; }
+        //public bool IsAdministrator { get; set; }
+
+        [Required(ErrorMessage = "Vennligst oppgi serienummer.")]
+        public string Serienummer { get; set; }
+
+        [Required(ErrorMessage = "Vennligst oppgi datoen ordren ble mottatt.")]
+        public DateTime MottattDato { get; set; }
+        [Required(ErrorMessage = "Vennligst skriv inn ordrenummer.")]
+        public string OrdreNummer { get; set; }
+
+        [Required(ErrorMessage = "Vennligst oppgi kunde email.")]
+        [EmailAddress(ErrorMessage = "Ikke en gyldig emailadresse.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Vennligst oppgi kundeadresse.")]
+        public string Adresse { get; set; }
+
+        [Required(ErrorMessage = "Vennligst oppgi kundetelefonnummer.")]
+        [Phone(ErrorMessage = "Ikke et gyldig telefonnummer.")]
+        public string TelefonNr { get; set; }
+
+        [Required(ErrorMessage = "Vennligst oppgi produkttype.")]
+        public string Produkttype { get; set; }
+
+        [Required(ErrorMessage = "Vennligst oppgi årsmodell.")]
+        public string Årsmodell { get; set; }
+
+        [Required(ErrorMessage = "Vennligst fyll ut service / rep garanti.")]
+        public string ServiceRep { get; set; }
     }
 }
     
