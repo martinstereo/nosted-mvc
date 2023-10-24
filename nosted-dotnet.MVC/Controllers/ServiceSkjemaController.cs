6c7a924c8df9561a802d27b9bf553db32b4305bb
mienord <mmnord@student.uia.no>
2023-10-24 17:52:00 +0200

    Gjorde Serviceskjema responsive,fjerna html encoding, fjerna tid på dato feltene

--- nosted-dotnet.MVC/Controllers/ServiceSkjemaController.cs
+++ nosted-dotnet.MVC/Controllers/ServiceSkjemaController.cs
@@ -1,54 +1,25 @@
 using nosted_dotnet.MVC.Models.ServiceSkjema;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-using System.Reflection;
-using System.Text.Encodings.Web;
-
-//In the GET request, it receives query string parameters,
-//encodes them to prevent XSS attacks, and displays them in a view model.
-//In the POST request, it receives form data, performs validation,
-//and logs the data before redirecting to another page.
 
 namespace nosted_dotnet.MVC.Controllers
 {
     public class ServiceSkjemaController : Controller
     {
-        private readonly HtmlEncoder _htmlEncoder;
-
-        public ServiceSkjemaController(HtmlEncoder htmlEncoder)
-        {
-            _htmlEncoder = htmlEncoder;
-        }
 
         // Dette er handlingsmetoden for å vise skjemaet
         public IActionResult Index(string Kunde, string MotattDato, string KundeMail, string KundeAdresse,
-                                  string KundeTelefonNr, string Serienummer, string Produkttype,
-                                  string Årsmodell, string ServiceRep, string OrdreNummer,
-                                  string AvtaltMedKunden, string RepBeskrivelse, string MedgåtteDeler,
-                                  decimal Arbeidstimer, string FerdigDato, string UtskiftetDelerRetur,
-                                  string ForsendelsesMåte, string SignaturKunde, string SignaturRep)
+            string KundeTelefonNr, string Serienummer, string Produkttype,
+            string Årsmodell, string ServiceRep, string OrdreNummer,
+            string AvtaltMedKunden, string RepBeskrivelse, string MedgåtteDeler,
+            decimal Arbeidstimer, string FerdigDato, string UtskiftetDelerRetur,
+            string ForsendelsesMåte, string SignaturKunde, string SignaturRep)
         {
-            // Hent alle offentlige egenskaper (properties) til denne kontrolleren
-            PropertyInfo[] properties = this.GetType().GetProperties();
-
-            // Gå gjennom egenskapene og krypter dem hvis de er strenger
-            foreach (var property in properties)
-            {
-                if (property.PropertyType == typeof(string))
-                {
-                    var value = (string)property.GetValue(this);
-                    if (value != null)
-                    {
-                        property.SetValue(this, _htmlEncoder.Encode(value));
-                    }
-                }
-            }
-
-            // Opprett en visningsmodell (view model) med de krypterte verdiene
+        
             var viewModel = new ServiceSkjemaViewModel
             {
                 Kunde = Kunde,
-                MotattDato = string.IsNullOrEmpty(MotattDato) ? DateTime.Now : DateTime.Parse(MotattDato),
+                MotattDato = string.IsNullOrEmpty(MotattDato) ? DateTime.Today : DateTime.Parse(MotattDato).Date,
                 KundeMail = KundeMail,
                 KundeAdresse = KundeAdresse,
                 KundeTelefonNr = KundeTelefonNr,
@@ -61,7 +32,7 @@ namespace nosted_dotnet.MVC.Controllers
                 RepBeskrivelse = RepBeskrivelse,
                 MedgåtteDeler = MedgåtteDeler,
                 Arbeidstimer = Arbeidstimer,
-                FerdigDato = string.IsNullOrEmpty(FerdigDato) ? DateTime.Now : DateTime.Parse(FerdigDato),
+                FerdigDato = string.IsNullOrEmpty(FerdigDato) ? DateTime.Today : DateTime.Parse(FerdigDato).Date,
                 UtskiftetDelerRetur = UtskiftetDelerRetur,
                 ForsendelsesMåte = ForsendelsesMåte,
                 SignaturKunde = SignaturKunde,
