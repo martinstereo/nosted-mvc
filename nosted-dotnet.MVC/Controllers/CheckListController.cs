0979aaddc4d9d54d2342ebeac4233ee4aff93f3b
mienord <mmnord@student.uia.no>
2023-10-25 15:26:19 +0200

    responsive CSS på sjekkliste

--- nosted-dotnet.MVC/Controllers/CheckListController.cs
+++ nosted-dotnet.MVC/Controllers/CheckListController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-using bacit_dotnet.MVC.Models.CheckList;
+using nosted_dotnet.MVC.Models.CheckList;
 using nosted_dotnet.MVC.Mappers;
 
 namespace nosted_dotnet.MVC.Controllers
@@ -12,8 +12,8 @@ namespace nosted_dotnet.MVC.Controllers
             var model = new CheckListViewModel
             {
                 CategoryGroups = new List<CheckListCategoryGroupModel> {
-                    new CheckListCategoryGroupModel {Name ="Mekanisk", Jobs=new List<string>{"Clutch Lameller", "Bremser|Bånd|Pal","Trommel Lager","Kjedestrammer","Wire", "Pinion Lager","Kjedehjul Kile"} },
-                    new CheckListCategoryGroupModel{ Name="Hydraulisk", Jobs=new List<string>{"Sylinder Lekasje","Slanger","Hydraulikkblokk Test","Oljeskifte på Tank","Oljeskifte på Girboks","Ringsylinder | Skift Tetninger","Bremsesylinder | Skift Tetninger" } },
+                    new CheckListCategoryGroupModel {Name ="Mekanisk", Jobs=new List<string>{"Clutch Lameller", "Bremser|BÃ¥nd|Pal","Trommel Lager","Kjedestrammer","Wire", "Pinion Lager","Kjedehjul Kile"} },
+                    new CheckListCategoryGroupModel{ Name="Hydraulisk", Jobs=new List<string>{"Sylinder Lekasje","Slanger","Hydraulikkblokk Test","Oljeskifte pÃ¥ Tank","Oljeskifte pÃ¥ Girboks","Ringsylinder | Skift Tetninger","Bremsesylinder | Skift Tetninger" } },
                     new CheckListCategoryGroupModel{ Name="Elektrisk", Jobs=new List<string>{"Ledningsnett","Radio", "Knappekasse" } },
                 },
                 CheckListId = 1
