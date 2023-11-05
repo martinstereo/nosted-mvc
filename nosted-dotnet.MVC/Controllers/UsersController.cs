bceadd4ba3a2aaf8f9654959c329a9406ca8157e
annetteab <annetteab98@gmail.com>
2023-11-05 12:52:25 +0100

    initial commit addUser

--- nosted-dotnet.MVC/Controllers/UsersController.cs
+++ nosted-dotnet.MVC/Controllers/UsersController.cs
@@ -1,49 +1,86 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Owin.BuilderProperties;
 using nosted_dotnet.MVC.Data;
 using nosted_dotnet.MVC.Data.User;
 using nosted_dotnet.MVC.Entites;
+using nosted_dotnet.MVC.Entities;
+using nosted_dotnet.MVC.Models.Ordre;
 using nosted_dotnet.MVC.Models.Users;
 
 namespace nosted_dotnet.MVC.Controllers
 {
-    
-        //[Authorize]
-        public class UsersController : Controller
+
+    //[Authorize]
+    public class UsersController : Controller
+    {
+        private readonly IUserRepository userRepository;    
+
+
+        public UsersController(IUserRepository userRepository)
         {
-            private readonly IUserRepository userRepository;
+            this.userRepository = userRepository;
+        }
 
-            public UsersController(IUserRepository userRepository)
+        [HttpGet]
+        public IActionResult Index()
+        {
+            // Hent brukerdata fra en datakilde (for eksempel en database)
+            var users = userRepository.GetUsers(); // Implementer metode for å hente brukere fra databasen
+
+            // Konverter brukerdataene til UserViewModels
+            var userViewModels = users.Select(user => new UserViewModel
             {
-                this.userRepository = userRepository;
-            }
-            [HttpGet]
-            public IActionResult Index(string? email)
+                Id = user.Id,
+                Fornavn = user.Fornavn,
+                Etternavn = user.Etternavn,
+                Email = user.Email,
+      //          IsAdmin = user.IsAdmin // Hent eller beregn isAdmin verdien basert på brukerens rolle
+            }).ToList();
+
+            return View(userViewModels); // Returner visningen med brukerdataene
+        }
+    
+        [HttpGet]
+        public IActionResult Add(string? email)
+        {
+            var brukerList = userRepository.GetUsers();
+            if (email != null)
             {
-                var model = new UserViewModel();
-                model.Users = userRepository.GetUsers();
-                if (email != null)
+                brukerList = new List<UserEntity>()
+                    .Where(user => user.Email != null && user.Email.Equals(email, StringComparison.InvariantCultureIgnoreCase))
+                    .ToList();
+                if (brukerList.Count == 0)
                 {
-                    var currentUser = model.Users.FirstOrDefault(x => x.Email == email);
-                    if (currentUser != null)
-                    {
+                   // TempData["ErrorMessage"] = "Ingen brukere ble funnet med den gitte e-postadressen.";
+                    return RedirectToAction("Add");
 
-                        model.Email = currentUser.Email;
-                        model.Fornavn = currentUser.Fornavn;
-                        model.Etternavn = currentUser.Etternavn;
-                        model.IsAdmin = userRepository.IsAdmin(currentUser.Email);
-                    }
                 }
-                return View(model);
             }
+    
+            var model = brukerList.Select(user => new UserViewModel
+            {
+                Id = user.Id,
+                Fornavn = user.Fornavn,
+                Etternavn = user.Etternavn,
+                Email = user.Email,
+                IsAdmin = userRepository.IsAdmin(user.Email)
+            }).ToList();
+            
+            return View("Add", model);
+
+        }
 
             [HttpPost]
-            public IActionResult Save(UserViewModel model)
-            {
-
+        public IActionResult Save(UserViewModel model)
+        {
+           // if (ModelState.IsValid)
+          //  {
                 UserEntity newUser = new UserEntity
                 {
                     Fornavn = model.Fornavn,
@@ -60,13 +97,42 @@ namespace nosted_dotnet.MVC.Controllers
                     userRepository.Add(newUser);
 
                 return RedirectToAction("Index");
+          //  }
+           // else
+           // {
+          //      return View(model);
+        //    }
+       }
+      
+        public IActionResult Edit(UsersEditViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var updatedUser = new UserEntity
+                {
+                    Id = model.User.Id,
+                    Fornavn = model.User.Fornavn,
+                    Etternavn = model.User.Etternavn,
+                    Email = model.User.Email,
+                };
+                var roles = new List<string>();
+                userRepository.Update(updatedUser, roles);
+
+                return RedirectToAction("Index", "Ordre");
             }
 
-            [HttpPost]
-            public IActionResult Delete(string email)
-            {
-                userRepository.Delete(email);
-                return RedirectToAction("Index");
-            }
-        }
+            // If we got this far, something failed, redisplay form
+            return View(model);
     }
+       
+                [HttpPost]
+        public IActionResult Delete(string email)
+        {
+            userRepository.Delete(email);
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
+
---
+++ nosted-dotnet.MVC/Models/Users/UsersEditViewModel.cs
@@ -0,0 +1,23 @@
+﻿using nosted_dotnet.MVC.Entites;
+using nosted_dotnet.MVC.Models.Ordre;
+using System.ComponentModel.DataAnnotations;
+
+namespace nosted_dotnet.MVC.Models.Users;
+
+public class UsersEditViewModel
+    {
+        public UsersEditViewModel()
+        {
+            UpsertModel = new List<UserEntity>();
+        }
+
+        public UserViewModel Id { get; }
+        public UserViewModel Fornavn { get; set; }
+        public UserViewModel Etternavn { get; set; }
+        public UserViewModel Email { get; set; }
+        public bool IsAdmin { get; set; }
+        public UserViewModel User { get; internal set; }
+        public List<UserEntity> UpsertModel { get; }
+        public UserEntity UpdatedUser { get; }
+}
+
