cd72b8eb5effcb0fb3919f3b6ec75ffe1e0b3a45
naausicaa <martinsteiro02@gmail.com>
2023-11-11 19:44:44 +0100

    uhhhhhh

--- nosted-dotnet.MVC.Tests/Controllers/OrdreControllerUnitTest.cs
+++ nosted-dotnet.MVC.Tests/Controllers/OrdreControllerUnitTest.cs
@@ -4,35 +4,23 @@ using nosted_dotnet.MVC.Controllers;
 using nosted_dotnet.MVC.Data;
 using nosted_dotnet.MVC.Data.ServiceSkjema;
 using nosted_dotnet.MVC.Entities;
+using nosted_dotnet.MVC.Models.Ordre;
 using nosted_dotnet.MVC.Repositories;
 
 namespace nosted_dotnet.MVC.Tests.Controllers
 {
     public class OrdreControllerUnitTest
     {
-        // private IAdresseRepository mockAdresseRepository;
+        private IAdresseRepository mockAdresseRepository;
         private IKundeRepository mockKundeRepository;
         private IProduktRepository mockProduktRepository;
         private IOrdreRepository mockOrdreRepository;
         private IServiceSkjemaRepository mockServiceSkjemaRepository;
         private ISjekklisteRepository mockSjekklisteRepository;
 
-        private void InitializeFakes()
-        {
-            mockKundeRepository = new Mock<IKundeRepository>().Object;
-            mockProduktRepository = new Mock<IProduktRepository>().Object;
-            mockOrdreRepository = new Mock<IOrdreRepository>().Object;
-            mockServiceSkjemaRepository = new Mock<IServiceSkjemaRepository>().Object;
-            mockSjekklisteRepository = new Mock<ISjekklisteRepository>().Object;
-        }
-
         private OrdreController GetUnitUnderTest()
         {
-            InitializeFakes();
-            return new OrdreController(mockKundeRepository, mockProduktRepository, mockOrdreRepository, mockServiceSkjemaRepository, mockSjekklisteRepository)
-            {
-
-            };
+            return new OrdreController(mockAdresseRepository, mockKundeRepository, mockProduktRepository, mockOrdreRepository, mockServiceSkjemaRepository, mockSjekklisteRepository);
         }
 
         [Fact]
@@ -41,15 +29,22 @@ namespace nosted_dotnet.MVC.Tests.Controllers
             // Arrange
             var OrdreControllerUnderTest = GetUnitUnderTest();
 
+            var ordre = mockOrdreRepository.GetAll();
+            var model = ordre.Select(o => new OrdreIndexViewModel
+            {
+                OrdreId = o.Id,
+                Fornavn = mockKundeRepository.Get(o.KundeId).Navn,
+                Etternavn = mockKundeRepository.Get(o.KundeId).Etternavn,
+                Type = mockProduktRepository.Get(o.ProduktId).Type,
+                RegNr = mockProduktRepository.Get(o.ProduktId).RegNr,
+                ServiceDato = o.ServiceDato
+            }).ToList();
 
             // Act
-
             var result = OrdreControllerUnderTest.Index() as ViewResult;
-            
+
             //Assert
-
-
-
+            Assert.IsType<OrdreIndexViewModel>(result.Model);
         }
 
     }
