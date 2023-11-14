8bfb1c9cc229c78db645478c6932410a87c616e1
annetteab <annetteab98@gmail.com>
2023-11-14 15:15:42 +0100

    lagt til bilde på reservedeler og utstyrsbehandling + redirect til login-page ved utlogging

---
+++ nosted-dotnet.MVC/Controllers/ReservedelerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace nosted_dotnet.MVC.Controllers
+{
+    public class ReservedelerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ nosted-dotnet.MVC/Controllers/Utstyrsbehandling.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace nosted_dotnet.MVC.Controllers
+{
+    public class Utstyrsbehandling : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
