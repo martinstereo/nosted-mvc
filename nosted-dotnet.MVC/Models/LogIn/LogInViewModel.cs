463fca9a0c1cdac977896241b5f1749b359f0d21
Busenur Yilmaz <busenury@uia.no>
2023-11-16 10:57:53 +0100

    har lagt LogInViewModel igjen. Vi trenger dette

---
+++ nosted-dotnet.MVC/Models/LogIn/LogInViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace nosted_dotnet.MVC.Models.LoggIn;
+
+public class LoginViewModel
+{
+    [Required(ErrorMessage = "Vennligst skriv inn brukernavn.")]
+    public string Brukernavn { get; set; }
+
+    [Required(ErrorMessage = "Vennligst skriv inn passord.")]
+    [DataType(DataType.Password)]
+    public string Passord { get; set; }
+}
